992af646aeccdaa1a843edf62768279eaab3df45
Taters <rollin.taters@gmail.com>
2026-01-21 02:17:54 +0300

    added data extraction script

---
+++ openfoam/extractAeroData.sh
@@ -0,0 +1,16 @@
+echo "U AOA BETA Cd Cl Cs CmRoll CmPitch CmYaw" > aeroResults.dat
+
+for CASE in U*/; do
+    AOA=$(echo $CASE | sed 's/.*_a\([-0-9]*\)_b.*/\1/')
+    BETA=$(echo $CASE | sed 's/.*_b\([-0-9]*\).*/\1/')
+
+    FC_FILE=$(ls $CASE/postProcessing/forceCoeffs/*/forceCoeffs.dat)
+
+    read Cd Cl Cs CmRoll CmPitch CmYaw <<< $(
+        awk 'END {print $2, $5, $11, $9, $8, $10}' "$FC_FILE"
+    )
+
+    echo "$U_MAG $AOA $BETA $Cd $Cl $Cs $CmRoll $CmPitch $CmYaw" \
+        >> aeroResults.dat
+done
+
